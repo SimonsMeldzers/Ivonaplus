5f5270e4a3ebf62fb96a845486e786e076b8df4f
Simon <fiewind1@gmail.com>
2023-01-05 18:19:48 +0200

    Created react router links for logo, serviss, login

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/header.css'
@@ -11,17 +12,17 @@ function Header() {
     <header>
     <Navbar id='header-body' expand="lg">
         <Container>
-            <Navbar.Brand href="#home">Ivonaplus</Navbar.Brand>
+            <Navbar.Brand href=""><Link className='router_logo' to='/'>Ivonaplus</Link></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#">Serviss</Nav.Link>
+                    <Nav.Link href=""><Link className='router_links' to='/'>Serviss</Link></Nav.Link>
                     <Nav.Link href="#">Auto noma</Nav.Link>
                     <Nav.Link href="#">Rezerves detaļas</Nav.Link>
-                    <Nav.Link href="#">Citas preces</Nav.Link>
+                    <Nav.Link href="">Citas preces</Nav.Link>
                 </Nav>
                 <Nav>
-                    <Nav.Link href="#">Log In</Nav.Link>
+                    <Nav.Link><Link className='router_links' to='/login'>Log In</Link></Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
