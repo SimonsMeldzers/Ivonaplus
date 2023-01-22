1aca23496e3e5fbab632d332c1fed5fccca8e3a8
Simon <fiewind1@gmail.com>
2023-01-22 14:27:38 +0200

    Added a post editing functionality to the CarRental page

--- src/components/rental_car.js
+++ src/components/rental_car.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/rental_car.css'
-import { Container, Row, Col, Image} from 'react-bootstrap';
+import { Container, Row, Col, Image, Spinner, Form, Button} from 'react-bootstrap';
 
-import { deleteDoc, doc } from 'firebase/firestore';
+import BlueButton from './blue_button';
+
+import Modal from 'react-bootstrap/Modal';
+
+import { updateDoc, deleteDoc, doc } from 'firebase/firestore';
 import { db } from '../firebase-config';
 
 function RentalCar({name, year, seats, doors, gearBox, AC, price, id, imageUrl, available}) {
@@ -11,6 +15,8 @@ function RentalCar({name, year, seats, doors, gearBox, AC, price, id, imageUrl, 
         await deleteDoc(postDoc);
         window.location.reload();
       };
+
+      const [modalShow, setModalShow] = React.useState(false);
 
   return (
     <div className='car_body'>
@@ -45,7 +51,7 @@ function RentalCar({name, year, seats, doors, gearBox, AC, price, id, imageUrl, 
                             <Col>
                                 <h1 className='car_price'>{price}€/24h</h1>
                                 <h6 className='car_price_difference'> *Cena var atšķirties</h6>
-                                {!available === "" ? <p className='available'>Pieejama</p> : <p className='not_available'>Nav pieejama</p>}
+                                {!available == "" ? <p className='available'>Pieejama</p> : <p className='not_available'>Nav pieejama</p>}
                             </Col>
                             <Col>
                                 <button id='blue_button'> Rezervēt </button>
@@ -56,6 +62,12 @@ function RentalCar({name, year, seats, doors, gearBox, AC, price, id, imageUrl, 
                                         }} 
                                     className='delete_button'>&#128465;
                                     </button>)}
+                                    {localStorage.getItem('isAuth') && (
+                                    <button 
+                                    onClick={() => setModalShow(true)}
+                                    className='delete_button'>&#9999;
+                                    </button>)}
+                                    
                             </Col>
                             
 
@@ -65,8 +77,184 @@ function RentalCar({name, year, seats, doors, gearBox, AC, price, id, imageUrl, 
             </Row>
                 
         </Container>
+        <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        id={id}
+        name={name}
+        year={year}
+        seats={seats}
+        doors={doors}
+        gearbox={gearBox}
+        ac={AC}
+        price={price}
+        available={available}
+      />
     </div>
   )
 }
+
+function MyVerticallyCenteredModal(props) {
+    const [name, setName] = useState(props.name);
+    const [year, setYear] = useState(props.year);
+    const [seats, setSeats] = useState(props.seats);
+    const [doors, setDoors] = useState(props.doors);
+    const [gearBox, setGearBox] = useState(props.gearbox);
+    const [AC, setAC] = useState(props.ac);
+    const [price, setPrice] = useState(props.price);
+    const [available, setAvailable] = useState(props.available);
+
+    const updatePost = async () => {
+        const postDoc = doc(db, "CarRental", props.id)
+        await updateDoc(postDoc, {
+            name,
+            year,
+            seats,
+            doors,
+            gearBox,
+            AC,
+            price,
+            available
+        });
+        await window.location.reload(); 
+    };
+
+    return (
+        
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {props.name}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form className='crForm'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Row>
+                        <Col>
+                            <Form.Label>Mašīnas nosaukums</Form.Label>
+                            <Form.Control onChange={(event) => {
+                                setName(event.target.value);
+                            }} 
+                            className='crInputDouble'
+                            type="" 
+                            placeholder="Mašīnas nosaukums" 
+                            defaultValue={props.name}
+                            />
+
+                        </Col>
+                        <Col>
+                            <Form.Label>Mašīnas Gads</Form.Label>
+                            <Form.Control onChange={(event) => {
+                                setYear(event.target.value);
+                            }} 
+                            className='crInputDouble'
+                            type="" 
+                            placeholder="Mašīnas Gads"
+                            defaultValue={props.year}
+                            />
+                        </Col>
+                    </Row>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Parametri</Form.Label>
+                    <Row>
+                        <Col>
+                            <Form.Control onChange={(event) => {
+                                setSeats(event.target.value);
+                            }} 
+                            className=''
+                                type="" 
+                                placeholder="Sēdvietas"
+                                defaultValue={props.seats} 
+                            />                 
+                        </Col>
+                        <Col>
+                            <Form.Control onChange={(event) => {
+                                setDoors(event.target.value);
+                            }} 
+                            className=''
+                                type="" 
+                                placeholder="Durvis"
+                                defaultValue={props.doors} 
+                            />   
+                        </Col>
+                        <Col>
+                            <Form.Select onChange={(event) => {
+                                setGearBox(event.target.value);
+                            }} 
+                            aria-label="Default select example"
+                            defaultValue={props.gearbox}>
+                                <option>Kārba</option>
+                                <option value="M">Manuālā</option>
+                                <option value="A">Automāts</option>
+                            </Form.Select>
+                        </Col>
+                        <Col>
+                            <Form.Select onChange={(event) => {
+                                setAC(event.target.value);
+                            }} 
+                            aria-label="Default select example"
+                            defaultValue={props.ac}>
+                                <option>A/C</option>
+                                <option value="+">Ir</option>
+                                <option value="-">Nav</option>
+                            </Form.Select>
+                        </Col>
+                     
+                    </Row>
+                    <Form.Label style={{marginTop: '15px'}}>Mašīnas cena par 24h</Form.Label>
+                    <Row >
+                        <Col>
+                            <Form.Control onChange={(event) => {
+                                setPrice(event.target.value);
+                            }} 
+                                className=''
+                                type="" 
+                                placeholder="Mašīnas cena"
+                                defaultValue={props.price} 
+                            /> 
+                        </Col>
+                        
+                        <Form.Check
+                            style={{marginLeft:'15px', marginTop:'15px'}} 
+                            onClick={() => {
+                                setAvailable("on");
+                            }} 
+                            inline
+                            label="Pieejama"
+                            name="group1"
+                            type="radio"
+                            id={`inline-radio-1`}
+                        />
+                        <Form.Check
+                            style={{marginLeft:'15px'}} 
+                            onClick={() => {
+                                setAvailable("");
+                            }} 
+                            inline
+                            label="Nepieejama"
+                            name="group1"
+                            type="radio"
+                            id={`inline-radio-2`}
+                        />
+                    </Row>
+
+                </Form.Group>
+                </Form>
+        </Modal.Body>
+
+        <Modal.Footer>
+            <Button id='blue_button' onClick={() => [props.onHide, updatePost()].map(func => func())}> Saglabāt </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 
 export default RentalCar;
